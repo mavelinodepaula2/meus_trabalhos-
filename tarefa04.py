2e531f8c8e79769fde68389b66fc71778ccffc9d
[mavelinodepaula2] <[matheusavelinodepaula07@gmail.com]>
2026-03-05 19:25:27 -0300

    tarefa03

---
+++ tarefa04.py
@@ -0,0 +1,13 @@
+import os 
+import time
+
+print("\033[?25l", end="")
+x = 1    
+tamanho, _ = os.get_terminal_size()
+tamanho = tamanho - 5
+while x <= tamanho - 5:
+    bar = chr(9608) * x + f" {x * 100 / tamanho:.0f}"
+    print(bar)
+    time.sleep(.1)
+    os.system('cls')
+    x = x + 1
